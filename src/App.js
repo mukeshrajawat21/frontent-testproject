a2086159a7d1c96279cce2aa74d7388c0b127031
mukeshrajawat21 <mukeshrajawat6@gmail.com>
2026-04-19 12:43:05 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to react</h2>
+      <h2>Welcome to react project</h2>
     </div>
   );
 }
